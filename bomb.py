680661eb6b2cf863dbd4adc857426e9fbe603f45
cianmcl05 <167808325+cianmcl05@users.noreply.github.com>
2024-05-01 08:22:05 -0400

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -51,7 +51,7 @@ def setup_phases():
     # bind the pushbutton to the LCD GUI so that its LED can be turned off when we quit
     gui.setButton(button)
     # setup the toggle switches thread
-    toggles = Toggles(component_toggles, 14, display_length=4)
+    toggles = Toggles(component_toggles, 8, display_length=4)
 
     # start the phase threads
     timer.start()
